1abe3a64fea5d62e82a6f48daff579ac951088cc
Anton Lechanka <anton.lechenko@banuba.com>
2017-10-25 14:12:20 +0300

    Update license

--- src/decoder.cpp
+++ src/decoder.cpp
@@ -1,5 +1,6 @@
 /*
    Copyright 2014-2015 Stanislav Ovsyannikov
+   Copyright 2017 Anton Lechanka
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
--- src/decoder.h
+++ src/decoder.h
@@ -1,6 +1,7 @@
 #pragma once
 /*
    Copyright 2014-2015 Stanislav Ovsyannikov
+   Copyright 2017 Anton Lechanka
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
